0a3cc5250d5e1b8e66339ad5bb0577507114e1cc
NathanDBrowne <nathan.browne1@gmail.com>
2023-05-02 20:36:27 +0100

    fix conversion util

--- fin_tools/formatting/utils.py
+++ fin_tools/formatting/utils.py
@@ -1,6 +1,6 @@
 import polars as pl
 
 
-def df_to_dict(df: pl.DataFrame) -> dict:
+def df_to_dict(df: pl.DataFrame, sort: str) -> dict:
     """convert dataframe to dict format required by plotly"""
-    return {k: v.to_list() for k, v in df.sort("tick_dir_imbal_bar_id").to_dict().items()}
+    return {k: v.to_list() for k, v in df.sort(sort).to_dict().items()}
